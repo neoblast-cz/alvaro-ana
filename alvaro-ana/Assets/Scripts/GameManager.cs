f3d646d0584b4fe9e39bcdefe1b6a7e64d45701d
David Huml <neoblast.cz@gmail.com>
2020-03-01 17:53:06 +0100

    movement, some basic triggers

---
+++ alvaro-ana/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    GameObject virtualCamera;
+    GameObject player;
+    Vector3 startPosition;
+    Quaternion startRotation;
+
+    int score;
+
+    void Start() {
+        if (instance != null){
+            Destroy(gameObject);
+        }
+        else {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        score = 0;
+
+        virtualCamera = GameObject.Find("MainVirtualCamera");
+        startPosition = virtualCamera.transform.position;
+        startRotation = virtualCamera.transform.rotation;
+    }
+
+    public void RestartLevel() {
+        virtualCamera.SetActive(false);
+        virtualCamera.transform.SetPositionAndRotation(startPosition, startRotation);
+        virtualCamera.SetActive(true);
+        player.transform.position = Vector3.zero;
+    }
+
+    public void AddScore(int addition)
+    {
+        score = score + addition;
+        Debug.Log(score);
+    }
+}
---
+++ alvaro-ana/Assets/Scripts/TriggerDead.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDead : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D target) {
+        if (target.tag == "Player") {
+            GameManager.instance.RestartLevel();
+        }
+    }
+}
